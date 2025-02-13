99680d0957562309c74050de42e164ff10bf60a1
KPD-LAB <87076015+Neovend@users.noreply.github.com>
2025-02-13 17:14:49 +0300

    add

---
+++ MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tabLayout: TabLayout = findViewById(R.id.tabLayout)
+        val viewPager: ViewPager = findViewById(R.id.viewPager)
+
+        val pagerAdapter = PagerAdapter(supportFragmentManager)
+        viewPager.adapter = pagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
+    }
+}
